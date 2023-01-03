6c4c9bce93ef6d6a66483a5b6d3abb55d4c8dca1
Sadra Teymourian <sadra.teymourian@gmail.com>
2023-01-03 20:36:34 +0330

    add homework3 files

---
+++ Homework/400422053/homework3/README.md
@@ -0,0 +1,15 @@
+# Assingnment #3
+
+This is a smart contract.
+
+
+1.  You can create a ColoredWallet.
+2.  You can exchange one blue coin for two red coins.
+3.  You send blue coins to other wallets.
+
+
+
+## Goerli Etherscan Link
+
+https://goerli.etherscan.io/address/0x0c2158ef0a0268665f78e33f88ac7539de41cd2c
+
---
+++ Homework/400422053/homework3/questionTwo.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MainContract {
+    struct ColoredWallet {
+        uint256 numberOfRedCoins;
+        uint256 numberOfBlueCoins;
+        address owner;
+        bool active;
+    }
+
+    mapping(address => ColoredWallet) public ownerToColoredWallet;
+
+    //Question 2 - A
+    function createColoredWallet () public {
+
+        // Check if the wallet already exists 
+        require(!ownerToColoredWallet[msg.sender].active , "You cannot create a new wallet when you already have one!");
+        ColoredWallet memory newColoredWallet = ColoredWallet(0, 100, msg.sender, true);
+        ownerToColoredWallet[msg.sender] = newColoredWallet;
+    }
+
+    //Question 2 - B
+    function getRedCoins () public {
+        // Check if the wallet exists 
+        require(ownerToColoredWallet[msg.sender].active , "You don not have any active wallet!");
+
+        //Check the number of blue coins 
+        require(ownerToColoredWallet[msg.sender].numberOfBlueCoins >= 1 , "You do not have enought blue coins!");
+
+        ColoredWallet memory foundColoredWallet = ownerToColoredWallet[msg.sender];
+        foundColoredWallet.numberOfBlueCoins = foundColoredWallet.numberOfBlueCoins - 1;
+        foundColoredWallet.numberOfRedCoins = foundColoredWallet.numberOfRedCoins + 2;
+        ownerToColoredWallet[msg.sender] = foundColoredWallet;
+    }
+
+    function showMyAddress () public view returns(address){
+        return msg.sender;
+    }
+
+
+    //Question 2 - C    
+    function sendBlueCoin(address _toWalletAddress, uint256 _numberOfBlueCoins) public{
+        // Check if the wallet both sender and receiver wallets exist   
+        require(ownerToColoredWallet[msg.sender].active , "You don not have any active wallet!");
+        require(ownerToColoredWallet[_toWalletAddress].active , "Couldn't find the receiver wallet!");
+
+        //Check the difference between sender and receiver
+        require(msg.sender != _toWalletAddress , "You cannot send blue coins to yourself!");
+
+
+        ColoredWallet memory senderColoredWallet = ownerToColoredWallet[msg.sender];
+
+        //Check the balance
+        require(senderColoredWallet.numberOfBlueCoins >= _numberOfBlueCoins , "You do not have enough blue coins!");
+        
+        senderColoredWallet.numberOfBlueCoins = senderColoredWallet.numberOfBlueCoins - _numberOfBlueCoins;
+
+        ColoredWallet memory receiverColoredWallet = ownerToColoredWallet[_toWalletAddress];
+        receiverColoredWallet.numberOfBlueCoins = receiverColoredWallet.numberOfBlueCoins + _numberOfBlueCoins;
+        ownerToColoredWallet[_toWalletAddress] = receiverColoredWallet;
+
+        ownerToColoredWallet[msg.sender] = senderColoredWallet;
+    }
+}
